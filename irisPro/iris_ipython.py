630638d33c86274ae4b2822e254a042e276c68f2
PurpleCoffee <rmb12134@strath.ac.uk>
2015-11-13 20:41:15 +0000

    K LOOK AT THE GIVEN DATA SET BEFORE DECICING ON CLASSIFICATION OR REGRESSION

--- irisPro/iris_ipython.py
+++ irisPro/iris_ipython.py
@@ -37,6 +37,27 @@ print(iris.feature_names);
 print(iris.target);
 print(iris.target_names);
 # The target represnts what we are going to predict
+# other terms for target: response, outcome, label and dependant variable
 # 0 - setosa 
 # 1 - veriscolor
 # 2 - virginica
+
+
+# Types of problems:
+####################
+# CLASSIFICATION --> the target or response being predicted is categorical! 
+# meaning that its values are in a finite unordered set
+
+# REGRESSION --> The response or target is ordered and continuous. Such as a price of a house
+# THIS IS THE ONE YOU WANT!
+
+# For this data set, the iris data set
+# How can we tell this is classification and not regression?
+# You need to understand how the date is encoded first 
+# Then decide if the response variable is suitable for 
+# regression or classification
+
+#The 0 1 and 2 represent unordered categories ...
+
+
+#
