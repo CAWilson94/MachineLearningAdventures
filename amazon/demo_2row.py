2ec55dc7769374b1a51a2460ea61bc97b8f10aca
PurpleCoffee <rmb12134@strath.ac.uk>
2015-11-15 14:36:57 +0000

    example going through linear regression and amazon data import with sampled data. Need to split up training and testing data however

--- amazon/demo_2row.py
+++ amazon/demo_2row.py
@@ -1,4 +1,43 @@
+# from sklearn.datasets.base import load_files
+# s1 = load_files()
 
-import matplotlib
-import numpy
-import sklearn
+# imports
+import pandas as pd
+import matplotlib.pyplot as plt
+from patsy.desc import INTERCEPT
+
+print "read in csv"
+# Read data into a data frame
+data = pd.read_csv('Advertising.csv', index_col=0);
+print "now to get shape of data..."
+print data.head()
+
+# print data.shape
+print data.shape
+# data.shape prints (n_samples,N_features)
+# where n starts from 0, so  we have 3 rows which should show as 2
+
+#create X and y:
+feature_cols = ['TV','Radio','Newspaper']
+X= data[feature_cols]
+y=data.Sales
+
+# follow the usual sklearn pattern: import, instantiate, fit
+
+#import
+from sklearn.linear_model import LinearRegression
+#instantiate
+lm = LinearRegression()
+#fit
+lm.fit(X, y)
+
+#print intercept and coefficients 
+print lm.intercept_
+print lm.coef_
+
+#pair feature names with coefficients
+print zip(feature_cols, lm.coef_)
+#predict for new observation
+print lm.predict([100, 25, 25])
+#calculate the R squared 
+print lm.score(X, y)
