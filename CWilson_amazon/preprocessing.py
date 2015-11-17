8b2dc2db0a2e4e28cc1d19a40a5ed7a0ebf2ad90
PurpleCoffee <rmb12134@strath.ac.uk>
2015-11-17 11:13:20 +0000

    final model, fucking variance

---
+++ CWilson_amazon/preprocessing.py
@@ -0,0 +1,28 @@
+import csv
+
+# Write training and testing files
+training = csv.writer(open("training.csv", "wb"))
+testing = csv.writer(open("testing.csv", "wb"))
+
+# Read in original file
+with open("pp-complete.csv", "rb") as f:
+    csvreader = csv.reader(f, delimiter=",")
+    # Split up data
+    for row in csvreader :
+        if "2015" in row[2]:
+            print "2015 spotted"
+            testing.writerow(row)
+        else:
+            print "writing to training"
+            training.writerow(row)
+            
+            
+
+            
+    
+
+    
+    
+    
+
+
